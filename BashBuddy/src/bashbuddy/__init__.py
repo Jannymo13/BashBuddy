0de5c7be5a066cf47075712d304ec89676035e5d
Jannymo13 <jonkonichottil@gmail.com>
2025-10-04 13:57:12 -0400

    feat: added help command

--- BashBuddy/src/bashbuddy/__init__.py
+++ BashBuddy/src/bashbuddy/__init__.py
@@ -1,2 +1,18 @@
-def test():
-    print("hello")
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("request")
+@click.option("--cmd", "-c", help="Command to get help for", required=False)
+def help(request: str, cmd: str | None):
+    if cmd:
+        click.echo(f"Help for command '{cmd}':")
+    click.echo(request)
+
+
+cli.add_command(help)
