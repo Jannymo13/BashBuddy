0166b14346ef420f5a46f6f2fb9740bb06fd6db1
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-04 15:37:10 -0400

    cleanup

--- web/server/app.py
+++ web/server/app.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/users')
+def users(): 
+    return {"users": ["Alice", "Bob", "Charlie"]}
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
